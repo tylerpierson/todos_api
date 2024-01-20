6e44a36d58f2a66fb71978c63502507efcdb42eb
Tyler Pierson <tylerpierson92@gmail.com>
2024-01-20 16:19:18 -0600

    Completed API with unit testing

--- tests/todo.test.js
+++ tests/todo.test.js
@@ -18,6 +18,7 @@ afterAll(async() => {
 })
 
 describe('Testing Todo Endpoints for RESTFUL JSON API', () => {
+    // Index Test
     test('It should explain a list of todos', async () => {
         const todo = new Todo({ title: 'test todo', description: 'test', completed: true })
         await todo.save()
@@ -35,6 +36,7 @@ describe('Testing Todo Endpoints for RESTFUL JSON API', () => {
         }
     })
 
+    // Create Test
     test('It should create a new todo', async() => {
         const response = await request(app).post('/todos').send({
             title: "my todo",
@@ -45,5 +47,42 @@ describe('Testing Todo Endpoints for RESTFUL JSON API', () => {
         expect(response.body.title).toEqual('my todo')
         expect(response.body.description).toEqual('things I need to do')
         expect(response.body.completed).toBeFalsy()
+    })
+
+    // Update Test
+    test('Given a valid body, it should update an existing todo and return it', async () => {
+        const todo = new Todo({ title: 'test todo', description: 'test', completed: true })
+        await todo.save()
+
+        const response = await request(app).put(`/todos/${todo._id}`).send({
+            description: "this is sparta"
+        })
+
+        expect(response.statusCode).toBe(200)  
+        expect(response.body.description).toEqual('this is sparta')
+    })
+
+    // Destroy Test
+    test('It should delete a pre-existing todo given a valid todo id', async () => {
+        const todo = new Todo({ title: 'test todo', description: 'test', completed: true })
+        await todo.save()
+
+        const response = await request(app).delete(`/todos/${todo._id}`)
+
+        expect(response.statusCode).toBe(200)  
+        expect(response.body.msg).toEqual(`The todo with the ID of ${todo._id} was deleted from the MongoDB database. No further action necessary.`)
+    })
+
+    // Show Test
+    test('It should show a pre-existing todo given a valid todo id', async () => {
+        const todo = new Todo({ title: 'test todo', description: 'test', completed: true })
+        await todo.save()
+
+        const response = await request(app).get(`/todos/${todo._id}`)
+
+        expect(response.statusCode).toBe(200)  
+        expect(response.body.title).toEqual('test todo')
+        expect(response.body.description).toEqual('test')
+        expect(response.body.completed).toBeTruthy()
     })
 })
