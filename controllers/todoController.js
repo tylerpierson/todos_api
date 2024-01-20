dae10853f7472fe409eca22ad718e754f4d2fd47
Tyler Pierson <tylerpierson92@gmail.com>
2024-01-20 10:09:15 -0600

    Set up controller file structure

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,70 +1,51 @@
 const Todo = require('../models/todo')
 
 // Index
-exports.indexTodo = async (req, res) => {
+exports.index = async function index (req, res) {
+    // Grab all of the todos
     try {
-        const foundTodos = await Todo.find({})
         
-        res.status(200).json({ message: 'Todo deleted successfully' })
-
     } catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-}
-
-// New
-exports.newTodo = (req, res) => {
-    res.render('todos/New')
-}
-
-// Destroy
-exports.deleteTodo = async (req, res) => {
-    try {
-        await Todo.findOneAndDelete({ '_id': req.params.id })
-        res.status(200).json({ message: 'Todo deleted successfully' })
-    } catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-}
-
-// Update
-exports.updateTodo = async (req, res) => {
-    if (req.body.completed === 'on') {
-        req.body.completed = true
-    } else {
-        req.body.completed = false
-    }
-
-    try {
-        const updatedTodo = await Todo.findOneAndUpdate({ '_id': req.params.id }, req.body, { new: true })
-        res.status(200).json(updatedTodo)
-    } catch (error) {
-        res.status(400).json({ message: error.message })
+        
     }
 }
 
 // Create
-exports.createTodo = async (req, res) => {
-    if (req.body.completed === 'on') {
-        req.body.completed = true
-    } else {
-        req.body.completed = false
-    }
-
+exports.create = async function create (req, res) {
+    // Make a new todo
     try {
-        const todo = await Todo.create(req.body)
-        res.status(200).json(todo)
+        
     } catch (error) {
-        res.status(400).json({ message: error.message })
+        
+    }
+}
+
+// Update
+exports.update = async function update (req, res) {
+    // Update a todo that was already made
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// Destroy
+exports.destroy = async function destroy (req, res) {
+    // Delete an existing todo
+    try {
+        
+    } catch (error) {
+        
     }
 }
 
 // Show
-exports.getTodo = async (req, res) => {
+exports.show = async function show (req, res) {
+    // Show 1 individual todo
     try {
-        const foundTodo = await Todo.findOne({ _id: req.params.id })
-        res.status(200).json(foundTodo)
+        
     } catch (error) {
-        res.status(400).json({ message: error.message })
+        
     }
 }
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router()
 const todoController = require('../controllers/todoController')
 
 // Index
-router.get('/', todoController.indexTodo)
+router.get('/', todoController.index)
 
 // Create
-router.post('/', todoController.createTodo)
+router.post('/', todoController.create)
 
 // Update
-router.put('/:id', todoController.updateTodo)
+router.put('/:id', todoController.update)
 
 // Destroy
-router.delete('/:id', todoController.deleteTodo)
+router.delete('/:id', todoController.destroy)
 
 // Show
-router.get('/:id', todoController.getTodo)
+router.get('/:id', todoController.show)
 
 module.exports = router
